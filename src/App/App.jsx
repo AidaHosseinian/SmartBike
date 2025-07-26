3d64e8478c5980a66165d1c8ce16ebf20b3580bc
Aida <aida.hosseinian@yahoo.com>
2025-07-26 15:23:09 +0200

    added Route for pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,14 +3,22 @@ import './app.scss'
 import Header from '../Components/header/Header.jsx'
 import Homepage from '../Components/Homepage/Homepage.jsx'
 import Footer from '../Components/footer/footer.jsx'
+import {BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 
 function App() { 
   return (
     <div className='main-container'>
-      <Header />
-      <Homepage />
-      <Footer />
+      <BrowserRouter >
+        <Header />
+        <Link to="/login">hsdfgh</Link>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="/login" element={<h1>Hello</h1>} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
